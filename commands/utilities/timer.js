e909e62966ba6175e0cf1bdbdfa367035e2658dc
Roel Taga <roeltaga@gmail.com>
2022-05-04 16:25:41 +0200

    Added JSON file to save the timers; Fixed an issue where using an invalig time letter like 8R would still convert it to 8s

--- commands/utilities/timer.js
+++ commands/utilities/timer.js
@@ -5,6 +5,8 @@ module.exports = {
     devOnly: false,
     run: async ({ client, prefix, message, args }) => {
 
+        const fs = require('fs');
+
         const argsArr = args.toLowerCase().trim().split(/ +/g)
 
         if (argsArr[0].length == 0) {
@@ -44,11 +46,16 @@ To set a quick timer in seconds you dont need to type "s" in the end.`)
                     timerText += argNumber + "s "
                     break;
                 default:
-                    totalTimeInSeconds += argNumber
-                    timerText += argNumber + "s "
+                    if (!isNaN(parseFloat(argLetter))) {
+                        totalTimeInSeconds += argNumber
+                        timerText += argNumber + "s "
+                    }
                     break;
             }
         });
+
+        //remove the extra space in the end
+        timerText = timerText.trimEnd()
 
         if (totalTimeInSeconds == 0) {
             message.reply(
@@ -59,6 +66,7 @@ You can choose to use one of them or all of them together.
 To set a quick timer in seconds you dont need to type "s" in the end.`)
         } else {
 
+            // SET TIMER
             const timerInMs = totalTimeInSeconds * 1000;
 
             message.channel.send(`${message.author.username} set timer for ${timerText}`);
@@ -67,6 +75,43 @@ To set a quick timer in seconds you dont need to type "s" in the end.`)
                 message.channel.send(`${message.author}, your timer is over`);
                 console.log(`Timer: ${message.author.tag}'s ${message.id} timer is over`);
             }, timerInMs);
+
+
+
+            let timerObject = {
+                "timer_guild": message.guildId,
+                "timer_channel": message.channelId,
+                "timer_message": message.id,
+                "timer_user": message.author.id,
+                "timer_created": message.createdTimestamp,
+                "timer_time": timerInMs,
+                "timer_text": timerText
+            }
+
+            // read timers.json
+            let timersRAW = fs.readFileSync("user_data/timers.json");
+            let timersJSON = JSON.parse(timersRAW);
+
+            // adding new timer object to array
+            timersJSON.push(timerObject)
+
+            // write to file: user_data/timers.json
+            timersRAW = JSON.stringify(timersJSON, null, 2)
+            fs.writeFileSync("user_data/timers.json", timersRAW)
+
+
+
+
+            // function timerConstructor(guildID, channelID, messageID, created, time, timeText) {
+            //     this.timer_guild = guildID;
+            //     this.timer_channel = channelID;
+            //     this.timer_message = messageID;
+            //     this.timer_created = created;
+            //     this.timer_time = time;
+            //     this.timer_text = timeText;
+            // }
+            // let timerObject = new timerConstructor("555", "asdf", "sadfasdf", "00000", "5s", "5 seconds bro")
+
         }
     }
 
