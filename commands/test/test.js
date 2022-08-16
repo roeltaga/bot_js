144805596ee95d4db999b801246b758ab5deb354
Roel Taga <roeltaga@gmail.com>
2022-08-16 13:05:34 +0200

    Resuming the project

--- commands/test/test.js
+++ commands/test/test.js
@@ -1,14 +1,44 @@
+
+const { clog } = require("../../proCode/proConsole.js")
+
 module.exports = {
     name: "test",
     category: "test",
     permissions: [],
     devOnly: true,
-    run: async ({client, message, args}) => {
+    run: async ({ client, message, args }) => {
 
-        const {clog} = require("../../proCode/proConsole.js")
-        
+        const trashencrypt = require("../../proCode/trashEncrypt")
 
-        
+        trashencrypt.num(args)
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // const timer = require("../utilities/timer")
+
+        // try {
+        //     await timer.run({ client, message, args })
+        // } catch (err) {
+        //     clog(err, "e")
+        // }
+
+
+
+
+
+
+
+
         clog(`Test: ${args}`, "s")
 
         message.react("☑️").then().catch(console.error)
