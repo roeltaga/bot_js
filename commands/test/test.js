bda589437c26c714364169c438025692e977a951
Roel Taga <roeltaga@gmail.com>
2022-08-23 12:15:57 +0200

    Testing date and time

--- commands/test/test.js
+++ commands/test/test.js
@@ -25,6 +25,10 @@ module.exports = {
 
         clog(`Test: ${args}`, "s")
 
+        let d = new Date()
+        clog(`Test TIME: ${d.getTime()}`, "a")
+        clog(`Test TIME: ${d.getHours()}:${d.getMinutes()}`, "a")
+
         // message.guild.channels.cache.get("897593299253211176").send(`Don't forget to \`/bump\` the server <@&1010317029028413500>`)
 
         message.react("☑️").then().catch(console.error)
