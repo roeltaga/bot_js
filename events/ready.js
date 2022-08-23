5d9c31c977a021f96fdf5d28473b15cebd8ad3b4
Roel Taga <roeltaga@gmail.com>
2022-08-23 16:22:22 +0200

    Added code to reload bump reminders on restart

--- events/ready.js
+++ events/ready.js
@@ -8,10 +8,14 @@ module.exports = {
         let { restoreTimers } = require("../onReload/restoreTimers.js")
 
         // try {
-            restoreTimers(bot)
+        restoreTimers(bot)
         // } catch (error) {
         //     console.log(error)
         // }
+
+        // Restore Bump reminder
+        const bump = require("../commands/utilities/bump")
+        bump.reload({ ...bot})
 
         clog("----------------", "s")
         clog("Bot Ready", "s")
