66f9e7c5b984d9365d13fbf5b5a5dd953b500d79
Roel Taga <roeltaga@gmail.com>
2022-08-25 02:56:09 +0200

    Added the onload code

--- commands/utilities/membercount.js
+++ commands/utilities/membercount.js
@@ -22,7 +22,7 @@ module.exports = {
                 // log to console
                 clog(`${message.author.tag} used /membercount, server: ${guild}, members: ${humans}`, "s")
             })
-            .catch( err => clog(err, "e"))
+            .catch(err => clog(err, "e"))
 
 
 
@@ -35,6 +35,71 @@ module.exports = {
         // message.reply(`There are \`${humans} Members\` and ${bots} Bots in this server, *${total} in total.*`)
 
         // clog(`${message.author.tag} used /membercount, server: ${guild}, members: ${humans}`, "s")
+
+
+    },
+
+    onload: async ({ client }) => {
+
+        // read data
+        let guildsDataRAW = fs.readFileSync("user_data/guilds.json");
+        let guildsDataJSON = JSON.parse(guildsDataRAW);
+        const guildID = Object.keys(guildsDataJSON)[0];
+        let memberCountData = guildsDataJSON[guildID].memberCount;
+
+        let lastUpdated = (new Date().getTime() - memberCountData.lastUpdated) / 60000;
+
+        // If channel name was updated > 5 mins ago, update it again
+        if (lastUpdated > 5) {
+            clog("Trying to update the members", "a")
+
+            setInterval(() => {
+                updateMemberCount()
+            }, 5 * 60 * 1000);
+
+
+        } else {
+            // if it has not been 5 minutes yet, set a timeout because the rate limit
+            clog(`Setting Timeout to update the members, (${5 - Math.round(lastUpdated)}m)`, "a")
+            setTimeout(() => {
+                setInterval(() => {
+                    updateMemberCount()
+                }, 5 * 60 * 1000);
+            }, (5 - lastUpdated) * 60 * 1000);
+        }
+
+        function updateMemberCount() {
+
+
+            client.guilds.fetch(guildID)
+                .then(guildObj => {
+                    const channelObj = guildObj.channels.cache.get(memberCountData.channelID);
+
+                    guildObj.members.fetch()
+                        .then(members => {
+                            const humans = members.filter(member => !member.user.bot).size
+
+                            channelObj.setName(`👥 Members: ${humans}`)
+                                .then(() => {
+
+                                    // write the data to the disk
+                                    memberCountData.lastUpdated = new Date().getTime()
+                                    guildsDataJSON[guildID].memberCount = memberCountData
+                                    guildsDataRAW = JSON.stringify(guildsDataJSON, null, 2)
+                                    fs.writeFileSync("user_data/guilds.json", guildsDataRAW)
+
+                                    clog(`memberCount: ${guildID} updated to: ${humans}`, "l")
+
+                                })
+                                .catch(err => clog(err, "e"))
+                        })
+                        .catch(err => clog(err, "e"))
+
+
+                })
+                .catch(err => clog(err, "e"))
+
+        }
 
 
     }
